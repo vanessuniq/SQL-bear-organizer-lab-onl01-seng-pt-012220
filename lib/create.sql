70d5eef8a37f0f17eedcb96ca79e4ca0f60e1b10
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-26 06:13:23 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE bears (id INTERGER PRIMARY KEY, name TEXT, age INTERGER, gender TEXT, color TEXT, temperament  TEXT, alive BOOLEAN);
+CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT, color TEXT, temperament  TEXT, alive BOOLEAN);
